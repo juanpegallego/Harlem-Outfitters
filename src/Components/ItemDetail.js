5858577f1ac48bab0939e19fc4296e968143813d
Juanchi <gallegojpe@gmail.com>
2022-01-12 05:30:19 -0600

    desafio 9 counter

--- src/Components/ItemDetail.js
+++ src/Components/ItemDetail.js
@@ -2,30 +2,56 @@ import React, { useContext, useState } from 'react'
 import ItemCount from './ItemCount.js'
 import './ItemDetail.scss'
 import { contexto } from './AppContext'
-
+import { Link } from 'react-router-dom'
 function ItemDetail({ title, id, price, stock, image, description }) {
 
     const [initial, setInitial] = useState(1);
+    const [showItem, setShowItem] = useState(true)
     const [selectedItemCount, setSelectedItemCount] = useState(0);
     const { agregarProducto } = useContext(contexto)
     const producto = { title, id, price, stock, image, description }
 
     const onAdd = (cantidad) => {
         agregarProducto(producto, cantidad)
+        setShowItem(false)
         if (initial > 0) {
             setSelectedItemCount(initial)
 
         }
     }
 
-
-    return (
+    if (showItem)
+        return (
+            <div className='item__detail__container'>
+                <div className='img__container'>
+                    <img src={image} />
+                    <p>{description}</p>
+                </div>
+                <div className='right__container'>
+                    <div className='title__container'>
+                        <h1>{title}</h1>
+                        <p>ID Producto:{id}</p>
+                        <h2>$ {price * 50}</h2>
+                        <h3>O en 3 cuotas de $ {(price * 50 / 3).toFixed(2)} con Visa/Mastercard</h3>
+                    </div>
+                    <ItemCount
+                        stock={stock}
+                        onAdd={onAdd}
+                        initial={initial}
+                        setInitial={setInitial}
+                    />
+                    <Link to={'/cart'}>
+                        <button className='btn'>Comprar</button>
+                    </Link>
+                </div>
+            </div>
+        )
+    else return (
         <div className='item__detail__container'>
             <div className='img__container'>
                 <img src={image} />
                 <p>{description}</p>
             </div>
-
             <div className='right__container'>
                 <div className='title__container'>
                     <h1>{title}</h1>
@@ -33,21 +59,11 @@ function ItemDetail({ title, id, price, stock, image, description }) {
                     <h2>$ {price * 50}</h2>
                     <h3>O en 3 cuotas de $ {(price * 50 / 3).toFixed(2)} con Visa/Mastercard</h3>
                 </div>
-                <ItemCount
-                    stock={stock}
-                    onAdd={onAdd}
-                    initial={initial}
-                    setInitial={setInitial}
-                />
-                <button className='btn'>Comprar</button>
+
+                <Link to={'/cart'}>
+                    <button className='btn'>Comprar</button>
+                </Link>
             </div>
-
-
-
-
-
-
-
         </div>
     )
 }
